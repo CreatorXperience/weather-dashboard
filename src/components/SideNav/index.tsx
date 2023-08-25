d89c68d04a08a055a1688b1ce6e3d1c3f2f3292b
CreatorXperience <allyearmustobey@gmail.com>
2023-08-25 04:42:19 +0100

    ft- implement side nav

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -1,10 +1,51 @@
 import { styled } from "styled-components";
 import { Icons } from "../../constants/icons/icons";
+import { useState, useRef } from "react";
 
 const SideNav = () => {
+  const [IsClicked, setIsClicked] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    setIsClicked((IsClicked) => !IsClicked);
+    ref.current?.classList.toggle(".movein");
+  };
   return (
     <SideNavWrapper>
-      <div className="container">{Icons.brand()}</div>
+      <div className="container">
+        <div className="brand">{Icons.brand()}</div>
+        <div className="icons">{Icons.homeIcon()}</div>
+        <div className="icons">{Icons.reportIcon()}</div>
+        <div className="icons">{Icons.profileIcon()}</div>
+        <div className="icons">{Icons.settingsIcon()}</div>
+        <div className="hamburger icon-sm" onClick={() => handleClick()}>
+          {Icons.hamburgerIcon()}
+        </div>
+      </div>
+
+      {/* TODO: remove IsClicked and make side nav slide out */}
+      {IsClicked ? (
+        <div className={`container-sm ${IsClicked ? "moveout" : ""}`} ref={ref}>
+          <div className="icons">
+            {Icons.brand()}
+            <p>Weather</p>
+          </div>
+          <div className="icons">
+            {Icons.homeIcon()} <p>Home</p>
+          </div>
+          <div className="icons">
+            {Icons.reportIcon()} <p>Report</p>{" "}
+          </div>
+          <div className="icons">
+            {Icons.profileIcon()} <p>Profile</p>
+          </div>
+          <div className="icons">
+            {Icons.settingsIcon()} <p>Settings</p>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </SideNavWrapper>
   );
 };
@@ -12,9 +53,90 @@ const SideNav = () => {
 export default SideNav;
 
 const SideNavWrapper = styled.div`
-  width: 10%;
+  width: 6%;
   .container {
     height: 100vh;
-    border: 1px solid red;
+    border: 1px solid #70707089;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+
+    .icons {
+      margin-top: 5rem;
+    }
+    .brand {
+      margin-top: 2rem;
+    }
+
+    .hamburger {
+      margin-top: 1rem;
+    }
+
+    .icon-sm {
+      display: none;
+    }
+  }
+
+  .container-sm {
+    display: none;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 40%;
+    .container {
+      height: 10vh;
+      border: 1px solid #70707089;
+
+      .icons,
+      .brand {
+        display: none;
+      }
+
+      .icon-sm {
+        display: block;
+      }
+    }
+
+    .moveout {
+      animation: move 0.2s ease-in-out;
+    }
+    @keyframes move {
+      0% {
+        transform: translate(-100%);
+      }
+
+      40% {
+        transform: translate(-60%);
+      }
+      60% {
+        transform: translate(-40%);
+      }
+      80% {
+        transform: translate(-20%);
+      }
+      100% {
+        transform: translate(0%);
+      }
+    }
+
+    .container-sm {
+      display: flex;
+      flex-direction: column;
+      align-items: start;
+      border: 1px solid #70707089;
+      width: 100%;
+      height: auto;
+
+      .icons {
+        display: flex;
+        align-items: center;
+        margin-left: 15px;
+        margin-top: 2rem;
+
+        p {
+          margin: 10px;
+        }
+      }
+    }
   }
 `;
--- src/constants/icons/icons.tsx
+++ src/constants/icons/icons.tsx
@@ -1,7 +1,27 @@
 import brandLogo from "../../assets/brand-logo.svg";
+import homeIcon from "../../assets/home-icon.svg";
+import reportIcon from "../../assets/report-icon.svg";
+import profileIcon from "../../assets/profile-icon.svg";
+import settingsIcon from "../../assets/settings.svg";
+import hamburgerIcon from "../../assets/hamburger.svg";
 const Icons = {
   brand: () => {
     return <img src={brandLogo} alt="brand logo" />;
+  },
+  homeIcon: () => {
+    return <img src={homeIcon} alt="home icon" />;
+  },
+  reportIcon: () => {
+    return <img src={reportIcon} alt="report icon" />;
+  },
+  profileIcon: () => {
+    return <img src={profileIcon} alt="profile icon" />;
+  },
+  settingsIcon: () => {
+    return <img src={settingsIcon} alt="settings icon" />;
+  },
+  hamburgerIcon: () => {
+    return <img src={hamburgerIcon} alt="hamburger icon" />;
   },
 };
 
